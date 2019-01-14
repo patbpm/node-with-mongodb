0d2620c1706b9bfe3128bd3284539383479cb178
patbpm <patbpm1@gmail.com>
2019-01-14 15:56:02 +0200

    Displaying our Website to Users

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require("express");
 var app = express();
 var port = 3000;
  
-app.get("/", (req, res) => {
- res.send("Hello World");
+app.use("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
  
 app.listen(port, () => {
